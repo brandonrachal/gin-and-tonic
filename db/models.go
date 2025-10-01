fc0ea9bb927864863042d14bce984fee598d4ac9
Brandon Rachal <brandon.rachal@gmail.com>
2025-10-01 07:49:08 -0500

    Fixed spacing typo

--- db/models.go
+++ db/models.go
@@ -4,5 +4,5 @@ type User struct {
 	Id        int    `db:"id" json:"id" form:"id"`
 	FirstName string `db:"first_name" json:"first_name" binding:"required"`
 	LastName  string `db:"last_name" json:"last_name" binding:"required"`
-	Email     string `db:"email"json:"email"  binding:"required"`
+	Email     string `db:"email" json:"email" binding:"required"`
 }
